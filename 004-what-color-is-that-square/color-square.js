119a10504675683876508f85e8588fe83f93b176
GiuseppePad17 <giuseppe17padula@gmail.com>
2024-08-06 14:59:09 +0200

    esercizio 004 incompleto

---
+++ 004-what-color-is-that-square/color-square.js
@@ -0,0 +1,8 @@
+console.log("hello");
+
+const row = ['1','2','3','4','5','6','7','8'];
+const columns = ['a','b','c','d','e','f','g','h'];
+
+const position = prompt("inserisci la posizione della riga").split('');
+console.log(position)
+
