4f6394eb8ffffa62ec5c542a1210521283f14a97
GiuseppePad17 <giuseppe17padula#gmail.com>
2024-10-02 17:21:20 +0200

    Miglioramento dell'esercizio Travel Manager svolto insieme a Simone

--- Step 2 - Practice!/final-project/travel-manager.js
+++ Step 2 - Practice!/final-project/travel-manager.js
@@ -1,35 +1,38 @@
-// Dichiaro le variabili che mi occorrono e le casto in Number per farci le operazioni
-let NumberOfDays = parseInt(prompt("Quanti giorni durerà il tuo viaggio?"));
-let availableBudget = parseFloat(prompt("Stabilisci quanto budget hai a disposizione per il tuo viaggio"));
+//dichiaro una variabile che mi indica se la Vacanza è in corso oppure no, per mantenere il software attivo
+let isVacationOn = true;
 
-console.log("Giorni del viaggio: "+NumberOfDays);
+//Stabilico il budget della mia vacanza
+const availableBudget = parseFloat(prompt("Stabilisci quanto budget hai a disposizione per il tuo viaggio"));
+
+
 console.log("Budget disponibile: "+availableBudget+"€")
 
+//Dichiaro le Variabili per le categorie (Trasporto, alloggio, pasto)
 let transportExpenses = 0;
 let accommodationExpenses = 0;
 let mealsExpenses = 0;
 
-// Effettuo il totale delle varie categorie di variabili Alloggio, trasporti ecc...
-for (let i = 1; i <= NumberOfDays; i++) {
-    transportExpenses += parseFloat(prompt("Quanti soldi hai speso per i TRASPORTI il giorno "+i+"?"));
-    accommodationExpenses += parseFloat(prompt("Quanti soldi hai speso per gli ALLOGGI il giorno "+i+"?"));
-    mealsExpenses += parseFloat(prompt("Quanti soldi hai speso per i PASTI il giorno "+i+"?"));
+
+while(isVacationOn === true){
+
+        console.log(isVacationOn);
+        
+        transportExpenses += parseFloat(prompt("Quanti soldi hai speso per i TRASPORTI il giorno ?"));
+        accommodationExpenses += parseFloat(prompt("Quanti soldi hai speso per gli ALLOGGI il giorno? "));
+        mealsExpenses += parseFloat(prompt("Quanti soldi hai speso per i PASTI il giorno ?"));
+
+        console.log("Soldi spesi per i TRASPORTI: "+transportExpenses+"€");
+        console.log("Soldi spesi per gli ALLOGGI: "+accommodationExpenses+"€");
+        console.log("Soldi spesi per i PASTI: "+mealsExpenses+"€");
+
+        let totalExpenditure = transportExpenses+accommodationExpenses+mealsExpenses;
+        console.log("Spesa totale: "+totalExpenditure+"€");
+
+        if (prompt("La vacanza è finita?")==="SI") {
+            isVacationOn = false;
+        }
+        
+    
 }
 
-// stampo tutto in console per avere un resoconto
-console.log("Soldi spesi per i TRASPORTI: "+transportExpenses+"€");
-console.log("Soldi spesi per gli ALLOGGI: "+accommodationExpenses+"€");
-console.log("Soldi spesi per i PASTI: "+mealsExpenses+"€");
 
-let totalExpenditure = transportExpenses+accommodationExpenses+mealsExpenses;
-console.log("Spesa totale: "+totalExpenditure+"€");
-window.alert("Hai speso un totale di:"+totalExpenditure+"€");
-
-// con una condizione verifico se ho superoto o meno il budget stampandolo a video
-if (availableBudget<totalExpenditure) {
-    window.alert("I soldi non sono bastati");
-    console.log("I soldi non sono bastati");
-} else {
-    window.alert("I soldi sono bastati");
-    console.log("I soldi sono bastati");
-}
